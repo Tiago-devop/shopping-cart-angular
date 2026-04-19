b420d2ebc5d8f392772fd40e3dce3027283fc268
Tiago Ramos Vasconcelos <tiagoramosvasconcelos.work@gmail.com>
2026-04-18 21:18:36 -0300

    feat(auth): implement login page with reactive forms and ngrx

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { login } from '../../../store/auth/auth.actions';
+import { selectAuthError, selectAuthLoading } from '../../../store/auth/auth.selectors';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private store = inject(Store);
 
+  isLoading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
+  showPassword = false;
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  ngOnInit(): void {
+    this.isLoading$ = this.store.select(selectAuthLoading);
+    this.error$ = this.store.select(selectAuthError);
+  }
+
+  get emailControl() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordControl() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    const { email, password } = this.loginForm.value;
+    this.store.dispatch(login({ email: email!, password: password! }));
+  }
 }
